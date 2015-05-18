229f3fd9a127f5d1ca560f1b971512b824dceb9d
Andrew M. Farrell <amfarrell@mit.edu>
2015-05-18 01:05:44 -0400

    add paths variable from tutorial.

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,13 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var react = require('gulp-react');
 var htmlreplace = require('gulp-html-replace');
+
+var path = {
+    HTML: 'src/index.html',
+    ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
+    JS: ['src/js/*.js', 'src/js/**/*.js'],
+    MINIFIED_OUT: 'build.min.js',
+    DEST_SRC: 'dist/src',
+    DEST_BUILD: 'dist/build',
+    DEST: 'dist'
+};
